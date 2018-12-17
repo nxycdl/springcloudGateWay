ef42eda56c6e43129d27ec3041f17e28c49c32b6
nxycdl <nxycdl@163.com>
2018-12-17 13:30:18 +0800

    first commit

---
+++ src/main/java/com/ycfd/SpringcloudGateWayApplication.java
@@ -0,0 +1,14 @@
+package com.ycfd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudGateWayApplication.class, args);
+	}
+
+}
+
